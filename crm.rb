c2f780578bf3a114059d3725da14ee0583f69c9f
NATHAN <nathan.ho28@gmail.com>
2014-10-31 16:20:35 -0400

    changed Contact class to DataMapper and implemented .gitignore

--- crm.rb
+++ crm.rb
@@ -1,8 +1,22 @@
 require_relative 'rolodex'
-require_relative 'contact'
-require 'sinatra'
 
-$rolodex = Rolodex.new
+require 'sinatra'
+require 'data_mapper'
+
+DataMapper.setup(:default, "sqlite3:database.sqlite3")
+
+class Contact
+	include DataMapper::Resource 
+
+	property :id, Serial 
+	property :first_name, String 
+	property :last_name, String 
+	property :email, String 
+	property :note, String
+end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
 
 get '/' do  #route
 	@crm_app_name = "My CRM"
